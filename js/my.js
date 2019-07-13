74114bf3ae3cb53479c617efe99c0b6d2f4f4709
Muhammadoff <futurecoder@gmail.com>
2019-07-13 16:50:43 +0500

    ikki

--- js/my.js
+++ js/my.js
@@ -24,71 +24,48 @@ $(document).ready(function() {
         translateY: chiqishQanot,
         easing: 'easeInOutSine'
     });
-});
 
-$(document).ready(function() {
-
-    // $(function() {
-    //     $(window).on('scroll', function() {
-    //         alert('scroll is working');
-
-    //         console.log("test");
-    //     });
+    // $('body').scroll(function() {
+    //     $(".sticky-head").addClass('none');
+    //     $(".sticky-head2").css("margin-top", "0");
 
     // });
 
-    $('body').scroll(function() {
-        $(".sticky-head").addClass('none');
-        $(".sticky-head2").css("margin-top", "0");
+    $(document).ready(function() {
+        $('body').bind('mousewheel', function(e) {
+            if (e.originalEvent.wheelDelta / 120 > 0) {
+                console.log('scrolling up !');
+                $(".sticky-head").addClass("block");
+                $(".sticky-head2").css("margin-top", "117px");
+            }
 
-        // if ($('.sticky-head2').css('margin-top' === '130')) {
-        //     $(".sticky-head").removeClass('none');
-        //     $(".sticky-head").addClass('d-block');
-        // } else {
 
-
-        //     $(".sticky-head").addClass('d-block');
-        // }
+            //  else if {
+            //     $(".sticky-head").removeClass("block");
+            //     $(".sticky-head2").removeCss("margin-top", "117px");
+            // }
+            else {
+                console.log('scrolling down !');
+                $(".sticky-head").addClass('none');
+                $(".sticky-head2").css("margin-top", "0");
+            }
+        });
     });
 
-    $("body").scrollTop(function() {
-        $(".sticky-head").removeClass('none');
-        $(".sticky-head").addClass('block');
+    // $("body").scrollTop(function() {
+    //     scrollTop = $(window).scrollTop();
+    //     $(".sticky-head".scrollTop).removeClass('none');
+    //     $(".sticky-head".scrollTop).addClass('block');
+    //     // $(".sticky-head2").css("margin-top", "130px");
+    // });
 
-        $('.sticky-head2').addClass("block");
-    });
-});
-
-
-// $(document).ready(function() {
-//     // Basic initialization is like this:
-//     // $('.your-class').slick();
-
-//     // I added some other properties to customize my slider
-//     // Play around with the numbers and stuff to see
-//     // how it works.
-//     $('.slick-carousel').slick({
-//         infinite: true,
-//         slidesToShow: 3, // Shows a three slides at a time
-//         slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
-//         arrows: true, // Adds arrows to sides of slider
-//         dots: true // Adds the dots on the bottom
-//     });
-// });
-
-$(document).ready(function() {
-
-    // SLIDER
-    //$('.slider').slick({});
-
-    $('.slider').slick({
-        dots: true,
-        speed: 1000,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        nextArrow: '<div class="slick-custom-arrow slick-custom-arrow-right"><i class="fas fa-angle-right"></i></div>',
-        prevArrow: '<div class="slick-custom-arrow slick-custom-arrow-left"><i class="fa fa-angle-left"></i></div>',
-    });
+    // $(window).scrollTop("click", function() {
+    //     $(".sticky-head").addClass("block");
+    //     $(".sticky-head2").css("margin-top", "117px");
+    // });
+    // $(window).scrollDown("click", function() {
+    //     $(".sticky-head").addClass("block");
+    //     $(".sticky-head2").css("margin-top", "117px");
+    // });
 
 });

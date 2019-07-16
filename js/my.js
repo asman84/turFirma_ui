304c5fcba79a93425fb0de646ea9f1fedd64df80
Muhammadoff <futurecoder@gmail.com>
2019-07-16 09:46:05 +0500

    index4 yangisi

--- js/my.js
+++ js/my.js
@@ -48,24 +48,105 @@ $(document).ready(function() {
                 console.log('scrolling down !');
                 $(".sticky-head").addClass('none');
                 $(".sticky-head2").css("margin-top", "0");
+                $(".sticky-head").addClass('none');
             }
         });
     });
 
-    // $("body").scrollTop(function() {
-    //     scrollTop = $(window).scrollTop();
-    //     $(".sticky-head".scrollTop).removeClass('none');
-    //     $(".sticky-head".scrollTop).addClass('block');
-    //     // $(".sticky-head2").css("margin-top", "130px");
-    // });
+});
 
-    // $(window).scrollTop("click", function() {
-    //     $(".sticky-head").addClass("block");
-    //     $(".sticky-head2").css("margin-top", "117px");
-    // });
-    // $(window).scrollDown("click", function() {
-    //     $(".sticky-head").addClass("block");
-    //     $(".sticky-head2").css("margin-top", "117px");
-    // });
 
+// $(document).ready(function() {
+
+//     $(".contain-newpage-itm2-img1").on("click", function() {
+//         $(".contain-newpage-itm2-img2").removeClass("show");
+//         $(".contain-newpage-itm2-img3").removeClass("show");
+//         $(".contain-newpage-itm2-img1").addClass("show");
+//     });
+//     $(".contain-newpage-itm2-img2").on("click", function() {
+//         $(".contain-newpage-itm2-img1").removeClass("show");
+//         $(".contain-newpage-itm2-img3").removeClass("show");
+//         $(".contain-newpage-itm2-img2").addClass("show");
+//     });
+//     $(".contain-newpage-itm2-img3").on("click", function() {
+//         $(".contain-newpage-itm2-img2").removeClass("show");
+//         $(".contain-newpage-itm2-img1").removeClass("show");
+//         $(".contain-newpage-itm2-img3").addClass("show");
+//     });
+
+// });
+
+
+$(document).ready(function() {
+
+    var turRegionBlockWidth = $(".contain-newpage").width();
+    var turRegionBlockPhoto = turRegionBlockWidth / 3;
+
+
+    $(".contain-newpage-itm2-img1").hover(function() {
+        $(".contain-newpage-itm2-img2").removeClass("img2");
+        $(".contain-newpage-itm2-img3").removeClass("img3");
+        $(".contain-newpage-itm2-img1").addClass("img1");
+    }, function() {
+        $(".contain-newpage-itm2-img1").removeClass("img1");
+    });
+    $(".contain-newpage-itm2-img2").hover(function() {
+        $(".contain-newpage-itm2-img1").removeClass("img1");
+        $(".contain-newpage-itm2-img3").removeClass("img3");
+        $(".contain-newpage-itm2-img2").addClass("img2");
+    }, function() {
+        $(".contain-newpage-itm2-img2").removeClass("img2");
+    });
+    $(".contain-newpage-itm2-img3").hover(function() {
+        $(".contain-newpage-itm2-img2").removeClass("img2");
+        $(".contain-newpage-itm2-img1").removeClass("img1");
+        $(".contain-newpage-itm2-img3").addClass("img3");
+    }, function() {
+        $(".contain-newpage-itm2-img3").removeClass("img3");
+    });
+
+
+
+});
+
+// $(".contain-newpage").hover(function() {
+//     anime({
+//         targets: ".contain-newpage-itm2",
+//         translateX: [790, 0],
+//         easing: 'easeInOutSine',
+//         duration: 2000
+//     });
+//     $(".contain-newpage-itm2").addClass("d-flex");
+//     $(".contain-newpage-itm").css("margin-right", "700px");
+//     $(".contain-newpage-itm2").removeClass("d-xl-none");
+// }, function() {
+//     anime({
+//         targets: ".contain-newpage-itm2",
+//         translateX: [0, 790],
+//         easing: 'easeInOutSine',
+//         duration: 2000
+//     });
+//     $(".contain-newpage-itm").css("margin-right", "0px");
+// });
+
+
+
+$(".contain-newpage").hover(function() {
+    anime({
+        targets: ".contain-newpage-itm2-img1",
+        translateX: [790, 0],
+        easing: 'easeInOutSine',
+        duration: 2000
+    });
+    $(".contain-newpage-itm2").addClass("d-flex");
+    $(".contain-newpage-itm").css("margin-right", "700px");
+    $(".contain-newpage-itm2").removeClass("d-xl-none");
+}, function() {
+    anime({
+        targets: ".contain-newpage-itm2",
+        translateX: [0, 790],
+        easing: 'easeInOutSine',
+        duration: 2000
+    });
+    $(".contain-newpage-itm").css("margin-right", "0px");
 });
